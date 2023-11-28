980ead48779d400835ebe41b23407eb1199e8895
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2023-11-27 16:19:20 -0800

    Implementation

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,7 +11,96 @@ class IsMoon implements StringChecker {
   }
 }
 
+class PalindromeChecker implements StringChecker {
+
+    @Override
+    public boolean checkString(String s) {
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+
+}
+
 public class TestListExamples {
+  
+  @Test
+  public void testFilterMultiple(){
+  
+    List<String> input = new ArrayList<>();
+    input.add("aba");
+    input.add("acca");
+
+    List<String> expected = new ArrayList<>();
+    expected.add("aba");
+    expected.add("acca");
+    
+    assertEquals(expected, ListExamples.filter(input, new PalindromeChecker()));
+    
+  }
+
+  @Test
+  public void testFilterNomatch(){
+
+    List<String> input = new ArrayList<>();
+    input.add("aaron");
+    input.add("brian");
+    input.add("david");
+    
+    List<String> expected = new ArrayList<>();
+    
+    assertEquals(expected, ListExamples.filter(input, new IsMoon()));
+    
+  }
+  
+  @Test
+  public void testFilterEmpty(){
+    
+    List<String> input = new ArrayList<>();
+    
+    List<String> expected = new ArrayList<>();
+
+    assertEquals(expected, ListExamples.filter(input, new IsMoon()));
+    
+  }
+
+  @Test
+  public void testFilterLarge(){
+
+    List<String> input = new ArrayList<>();
+    for(int i = 0; i < 100000; i++){
+      input.add("moon");
+    }
+
+      List<String> expected = new ArrayList<>();
+      for(int i = 0; i < 100000; i++){
+        expected.add("moon");
+      }
+      
+      assertEquals(expected, ListExamples.filter(input, new IsMoon()));
+    }
+    
+    @Test
+    public void testMergeLater(){
+      
+      
+      List<String> input = new ArrayList<>();
+      input.add("aaron");
+      input.add("brian");
+      input.add("david");
+      
+      List<String> input2 = new ArrayList<>();
+      input2.add("denice");
+      input2.add("fred");
+      
+      List<String> expected = new ArrayList<>();
+    expected.add("aaron");
+    expected.add("brian");
+    expected.add("david");
+    expected.add("denice");
+    expected.add("fred");
+    
+    assertEquals(expected, ListExamples.merge(input, input2));
+  }
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
@@ -18,4 +109,46 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+  
+  @Test(timeout = 500)
+  public void testMergeEmpty(){
+
+    List<String> input = new ArrayList<>();
+    
+    List<String> input2 = new ArrayList<>();
+    
+    List<String> expected = new ArrayList<>();
+    
+    assertEquals(expected, ListExamples.merge(input, input2));
+
+  }
+
+  @Test(timeout = 500)
+  public void testMergeSorted() {
+    
+    List<String> left = Arrays.asList("a", "b", "c");
+
+    List<String> right = Arrays.asList("d", "e", "e", "f");
+
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "e", "f");
+
+    assertEquals(expected, ListExamples.merge(left, right));
+  }
+
+  @Test(timeout = 500)
+  public void testMergeInverted(){
+
+    List<String> left = Arrays.asList("d", "e", "e", "f");
+
+    List<String> right = Arrays.asList("a", "b", "c");
+
+    List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "e", "f");
+
+    assertEquals(expected, ListExamples.merge(left, right));
+
+  }
 }
+
+
+
+
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,49 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# Check for the file structure
+
+if [[ ! -f "student-submission/ListExamples.java" ]]
+then
+    echo "-------------------------------------------"
+    echo "Could not find ListExamples.java. Check your directory structure and try again."
+    exit 1
+fi
+
+cp TestListExamples.java grading-area
+cp student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+javac -J-Duser.language=en -cp $CPATH *.java 2> compile.out
+
+if [[ $? -ne 0 ]]
+then
+    echo "-------------------------------------------"
+    echo "Could not compile the student's code. Error:"
+    echo
+    cat compile.out
+    exit 1
+fi
+
+echo "-------------------------------------------"
+echo "Compiled successfully. Running tests..."
+
+echo "-------------------------------------------"
+echo "Test results:"
+echo 
+
+java -cp $CPATH:. org.junit.runner.JUnitCore TestListExamples > test.out
+
+if [[ $? -ne 0 ]]
+then
+    echo "Student's code failed:"
+    echo
+    grep "Failures: " test.out
+else
+    echo "Student's code passed:"
+    echo
+    grep "OK" test.out
+fi
